ce6aff01a8b030d71f9b5881b49f451257ec1db6
Tim45363 <kasymovty@gmail.com>
2021-08-04 14:17:27 +0300

    finite commite

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ListOfLinksService} from "./shared/list-of-links.service";
+import {HttpServiceService} from "./shared/http-service.service";
+import {AccountRepositoryService} from "./shared/account-repository.service";
+import {DrawerFeedService} from "./shared/drawer-feed.service";
+import {DrawerStuffBox} from "./shared/DrawerStuufBox";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  encapsulation : ViewEncapsulation.None,
+
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor(public ListOfLinks: ListOfLinksService,
+              public http:HttpServiceService,
+              public account: AccountRepositoryService,
+              public DrawerFeed: DrawerFeedService) {
+  }
+
+  public drawerFeedBox = DrawerStuffBox;
+
+  ngOnInit(): void {
+    this.DrawerFeed.StartSubscription()
+  }
+
 }
